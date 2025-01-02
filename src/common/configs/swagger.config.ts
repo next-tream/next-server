160dd1a42bb04ac07807dc42ba637ea5b9825f58
지승 <rjs8833@gmail.com>
2025-01-02 16:11:13 +0900

    fixed cicd error

--- src/common/configs/swagger.config.ts
+++ src/common/configs/swagger.config.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const SwaggerConfig = new DocumentBuilder()
 	.setTitle('Nextream')
 	.setDescription('Nextream API 명세서')
-	.setVersion('1.1')
+	.setVersion('1.2')
 	.addBearerAuth()
 	.addBasicAuth()
 	.build();
