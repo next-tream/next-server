224114b179510b505d6899d134f34caadd969e34
지승 <rjs8833@gmail.com>
2025-01-02 16:03:57 +0900

    please... ec2 error

--- src/common/configs/swagger.config.ts
+++ src/common/configs/swagger.config.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const SwaggerConfig = new DocumentBuilder()
 	.setTitle('Nextream')
 	.setDescription('Nextream API 명세서')
-	.setVersion('1.0')
+	.setVersion('1.1')
 	.addBearerAuth()
 	.addBasicAuth()
 	.build();
