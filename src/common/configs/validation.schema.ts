237b4b6d401197d82ca98e9540aac849c79c67ce
지승 <rjs8833@gmail.com>
2025-01-02 14:58:26 +0900

    add zoi with sub domain:

--- src/common/configs/validation.schema.ts
+++ src/common/configs/validation.schema.ts
@@ -21,4 +21,5 @@ export const validationSchema = Joi.object({
 	KAKAO_URL: Joi.string().required(),
 	AWS_S3_ACCESS_KEY: Joi.string().required(),
 	AWS_S3_SECRET_ACCESS_KEY: Joi.string().required(),
+	SUB_DOMAIN: Joi.string().required(),
 });
