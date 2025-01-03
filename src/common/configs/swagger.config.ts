f6b41df6344b64ba29ece9ba6a7b079b2e6e860a
지승 <rjs8833@gmail.com>
2025-01-03 20:18:44 +0900

    hello world

--- src/common/configs/swagger.config.ts
+++ src/common/configs/swagger.config.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const SwaggerConfig = new DocumentBuilder()
 	.setTitle('Nextream')
 	.setDescription('Nextream API 명세서')
-	.setVersion('1.2')
+	.setVersion('1.3')
 	.addBearerAuth()
 	.addBasicAuth()
 	.build();
